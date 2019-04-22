ec10441a598fa71c77822473ec0ca4ff07b88bf1
ameed <ameedkabha@gmail.com>
2019-04-22 13:39:45 +0300

    t

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+console.log("hellow world!");
+console.log("hellow world!");
+console.log("hellow world!");
+let pElement = document.getElementById('ha1');
+pElement.innerHTML = 'we change inner';
