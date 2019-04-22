f80cec2caf613b25fb226f5d1a554a4c84e700a6
ameed <ameedkabha@gmail.com>
2019-04-22 14:02:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 
 console.log("hellow world!");
-console.log("hellow world!");
-console.log("hellow world!");
+console.log("hellow lord!");
+console.log("hellow king!");
 let pElement = document.getElementById('ha1');
 pElement.innerHTML = 'we change inner';
+let popo = document.getElementById('lolo');
+popo.innerHTML = 'helloo aaup';
